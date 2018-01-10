988a53bf48b3b89724c85f6c26d09d2aedbc2d4e
G <G@Laptop.lakeheadu.ca>
2018-01-10 16:13:32 -0500

    added literally everything we've done

--- WandSwattersMain/src/wandswattersmain/WandSwattersMain.java
+++ WandSwattersMain/src/wandswattersmain/WandSwattersMain.java
@@ -15,7 +15,7 @@ public class WandSwattersMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("let me out");
     }
     
 }
